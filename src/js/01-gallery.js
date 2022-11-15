b1b5dd887b947705fe3ecd92df91bc5ae58ab7d4
Vladislav Mararenko <mararenko.vladislav@gmail.com>
2022-11-15 13:09:59 +0200

    HW 8 first task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const gallerySettingsObject = {
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+
+const makeGalleryMarkup = () => {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`;
+    })
+    .join('');
+};
+
+const render = () => {
+  galleryList.insertAdjacentHTML('beforeend', makeGalleryMarkup());
+};
+
+render();
+
+let gallery = new SimpleLightbox('.gallery a', gallerySettingsObject);
