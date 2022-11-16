63cf561b7baa59b71620712549cb1002a6b59be0
Vladislav Mararenko <mararenko.vladislav@gmail.com>
2022-11-16 17:00:59 +0200

    HW second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const videoCurrentTimeKey = 'videoplayer-current-time';
+
+if (localStorage.getItem(videoCurrentTimeKey)) {
+  player.setCurrentTime(localStorage.getItem(videoCurrentTimeKey));
+}
+
+const saveVideoTime = data => {
+  localStorage.setItem(videoCurrentTimeKey, data.seconds);
+};
+
+player.on('timeupdate', throttle(saveVideoTime, 1000));
