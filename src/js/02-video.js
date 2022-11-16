b77006f2c93012e01d9b6485a232d6f157084140
Vladislav Mararenko <mararenko.vladislav@gmail.com>
2022-11-16 19:37:18 +0200

    HW 8 third task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,14 +3,14 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const videoCurrentTimeKey = 'videoplayer-current-time';
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
 
-if (localStorage.getItem(videoCurrentTimeKey)) {
-  player.setCurrentTime(localStorage.getItem(videoCurrentTimeKey));
+if (localStorage.getItem(CURRENT_TIME_KEY)) {
+  player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY));
 }
 
 const saveVideoTime = data => {
-  localStorage.setItem(videoCurrentTimeKey, data.seconds);
+  localStorage.setItem(CURRENT_TIME_KEY, data.seconds);
 };
 
 player.on('timeupdate', throttle(saveVideoTime, 1000));
---
+++ src/js/03-feedback.js
@@ -0,0 +1,47 @@
+import throttle from 'lodash.throttle';
+
+const FEEDBACK_KEY = 'feedback-form-state';
+const feedbackForm = document.querySelector('.feedback-form');
+let feedbackFormData = {
+  email: '',
+  message: '',
+};
+
+const renderFromLocalStorage = () => {
+  if (!JSON.parse(localStorage.getItem(FEEDBACK_KEY))) {
+    return;
+  }
+
+  feedbackFormData = JSON.parse(localStorage.getItem(FEEDBACK_KEY));
+
+  feedbackForm.elements.email.value = feedbackFormData.email;
+  feedbackForm.elements.message.value = feedbackFormData.message;
+};
+
+const onInputForm = () => {
+  feedbackFormData.email = feedbackForm.elements.email.value;
+  feedbackFormData.message = feedbackForm.elements.message.value;
+
+  localStorage.setItem(FEEDBACK_KEY, JSON.stringify(feedbackFormData));
+};
+
+const onSubmitForm = event => {
+  event.preventDefault();
+
+  if (
+    !feedbackForm.elements.email.value ||
+    !feedbackForm.elements.message.value
+  ) {
+    alert('Все поля должны быть заполнены!');
+    return;
+  }
+
+  feedbackForm.reset();
+  localStorage.removeItem(FEEDBACK_KEY);
+  console.log(feedbackFormData);
+};
+
+renderFromLocalStorage();
+
+feedbackForm.addEventListener('submit', onSubmitForm);
+feedbackForm.addEventListener('input', throttle(onInputForm, 500));
